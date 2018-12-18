b4a07270d2935e174696e827ccc74b7af783592b
YangYun <1186584388@qq.com>
2018-12-18 17:29:48 +0800

    Update train.py

--- train.py
+++ train.py
@@ -11,9 +11,14 @@
 #
 #================================================================
 
+import numpy as np
 import tensorflow as tf
+from PIL import Image
 from core import utils
 
+sess = tf.Session()
+input_shape = [416, 416]
+classes = utils.read_coco_names('./data/coco.names')
 file_pattern = "./data/train_data/train_*.tfrecords"
 
 def parser(serialized_example):
@@ -43,12 +48,17 @@ dataset = dataset.map(parser, num_parallel_calls = 10)
 dataset = dataset.repeat().batch(1).prefetch(1)
 iterator = dataset.make_one_shot_iterator()
 image, bboxes, labels = iterator.get_next()
+image, bboxes, labels = iterator.get_next()
 
-
-sess = tf.Session()
-
+# image, bboxes = utils.resize_image_correct_bbox(image, bboxes, input_shape)
+result = sess.run([image, bboxes, labels])
 result = sess.run([image, bboxes, labels])
 
 
+result = sess.run([image, bboxes, labels])
+image = Image.fromarray(np.uint8(result[0][0]))
+boxes = result[1][0]
+labels = result[2][0]
+scores = np.ones_like(labels)
 
-
+utils.draw_boxes(image, boxes, scores, labels, classes)
