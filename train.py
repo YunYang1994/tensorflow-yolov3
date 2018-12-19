1c1a99f842538e566a48200b846a9ad3aa83585c
YunYang1994 <YunYang1994@github.com>
2018-12-19 15:03:36 +0800

    train

--- train.py
+++ train.py
@@ -13,4 +13,13 @@
 
 # please refer to quick_train.py, I'll finish it soon
 
+# The question now I have to face is about my limited computational resources!
+# haha, if any substantial computational resources are avaliable to you,
+# welecome to clone this repository to train COCO dataset.
+# I‘ ll appreciate it if I am informed by any results. Thanks you a lot.
+
+# my Email is dreameryangyun@sjtu.edu.cn
+# looking forward to your messeage!
+
+
 
