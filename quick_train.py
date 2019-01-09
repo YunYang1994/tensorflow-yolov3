0baa9e81a90b59fe2d2cd3baff2f753c97982eb1
YunYang1994 <YunYang1994@github.com>
2019-01-09 21:00:54 +0800

    fuck yolov3

--- quick_train.py
+++ quick_train.py
@@ -25,9 +25,9 @@ LR = 0.0001
 SHUFFLE_SIZE = 1
 
 sess = tf.Session()
-num_classes = 20
-classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
-file_pattern = "../voc_tfrecords/voc*.tfrecords"
+num_classes = 80
+# classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+file_pattern = "../COCO/tfrecords/coco*.tfrecords"
 anchors = utils.get_anchors('./data/yolo_anchors.txt') # numpy [9,2]
 
 dataset = tf.data.TFRecordDataset(filenames = tf.gfile.Glob(file_pattern))
@@ -36,22 +36,29 @@ dataset = dataset.repeat().shuffle(SHUFFLE_SIZE).batch(BATCH_SIZE).prefetch(BATC
 iterator = dataset.make_one_shot_iterator()
 example = iterator.get_next()
 
-images, y_true = example
+layer = 0
+_ANCHORS = [anchors[6:9], anchors[3:6], anchors[0:3]]
+images, *y_true = example
 model = yolov3.yolov3(num_classes)
 with tf.variable_scope('yolov3'):
     y_pred = model.forward(images, is_training=False)
-    loss = model.compute_loss(y_pred, y_true)
-
+    loss = model.loss_layer(y_pred[layer], y_true[layer], _ANCHORS[layer], 0.5, 8)
 
 sess.run(tf.global_variables_initializer())
 
-pretrained_weights = tf.global_variables(scope="yolov3/darknet-53")
+# pretrained_weights = tf.global_variables(scope="yolov3/darknet-53")
+# load_op = utils.load_weights(var_list=pretrained_weights,
+                            # weights_file="../yolo_weghts/darknet53.conv.74")
+
+pretrained_weights = tf.global_variables(scope="yolov3")
 load_op = utils.load_weights(var_list=pretrained_weights,
-                            weights_file="../yolo_weghts/darknet53.conv.74")
+                            weights_file="../yolo_weghts/yolov3.weights")
 sess.run(load_op)
 
-result = sess.run([loss])
-
+result = sess.run([y_pred, y_true, loss])
+data = result[-1]
+print(np.sum(data[0]))
+print(np.sum(data[2]))
 
 
 
