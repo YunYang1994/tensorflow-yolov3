101e4a4f914f3e2bba2758e6aa318aabd704a5c3
YunYang1994 <YunYang1994@github.com>
2019-01-22 13:19:54 +0800

    voc

--- scripts/make_voc_tfrecords.sh
+++ scripts/make_voc_tfrecords.sh
@@ -14,6 +14,6 @@
 python scripts/extract_voc.py --voc_path /home/yang/test/VOC/train/ --dataset_info_path ./
 cat ./2007_train.txt ./2007_val.txt > voc_train.txt
 python scripts/extract_voc.py --voc_path /home/yang/test/VOC/test/ --dataset_info_path ./
-cat ./2007_train.txt ./2007_val.txt > voc_test.txt
+cat ./2007_test.txt > voc_test.txt
 python core/convert_tfrecord.py --dataset_txt ./voc_train.txt --tfrecord_path_prefix /home/yang/test/VOC/train/voc_train
 python core/convert_tfrecord.py --dataset_txt ./voc_test.txt  --tfrecord_path_prefix /home/yang/test/VOC/test/voc_test
