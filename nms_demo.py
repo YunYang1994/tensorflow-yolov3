720d36c816802a88c05087c3d79c11f4f1c7ab92
YunYang1994 <YunYang1994@github.com>
2019-01-26 01:31:18 +0800

    nms

--- nms_demo.py
+++ nms_demo.py
@@ -18,7 +18,7 @@ from PIL import Image
 from core import utils
 
 
-SIZE = [416, 416]
+IMAGE_H, IMAGE_W = 416, 416
 EPOCHS = 5
 # SIZE = [608, 608]
 classes = utils.read_coco_names('./data/coco.names')
@@ -26,7 +26,7 @@ num_classes = len(classes)
 image_path = "./data/demo_data/car.jpg"
 # image_path = "./data/demo_data/611.jpg"
 img = Image.open(image_path)
-img_resized = np.array(img.resize(size=tuple(SIZE)), dtype=np.float32)
+img_resized = np.array(img.resize(size=(IMAGE_H, IMAGE_W)), dtype=np.float32)
 img_resized = img_resized / 255.
 cpu_nms_graph, gpu_nms_graph = tf.Graph(), tf.Graph()
 
@@ -38,7 +38,7 @@ with tf.Session(graph=gpu_nms_graph) as sess:
         start = time.time()
         boxes, scores, labels = sess.run(output_tensors, feed_dict={input_tensor: np.expand_dims(img_resized, axis=0)})
         print("=> nms on gpu the number of boxes= %d  time=%.2f ms" %(len(boxes), 1000*(time.time()-start)))
-    image = utils.draw_boxes(img, boxes, scores, labels, classes, SIZE, show=True)
+    image = utils.draw_boxes(img, boxes, scores, labels, classes, [IMAGE_H, IMAGE_W], show=True)
 # nms on CPU
 input_tensor, output_tensors = utils.read_pb_return_tensors(cpu_nms_graph, "./checkpoint/yolov3_cpu_nms.pb",
                                            ["Placeholder:0", "concat_9:0", "mul_6:0"])
@@ -48,7 +48,7 @@ with tf.Session(graph=cpu_nms_graph) as sess:
         boxes, scores = sess.run(output_tensors, feed_dict={input_tensor: np.expand_dims(img_resized, axis=0)})
         boxes, scores, labels = utils.cpu_nms(boxes, scores, num_classes, score_thresh=0.5, iou_thresh=0.5)
         print("=> nms on cpu the number of boxes= %d  time=%.2f ms" %(len(boxes), 1000*(time.time()-start)))
-    image = utils.draw_boxes(img, boxes, scores, labels, classes, SIZE, show=True)
+    image = utils.draw_boxes(img, boxes, scores, labels, classes, [IMAGE_H, IMAGE_W], show=True)
 
 
 
