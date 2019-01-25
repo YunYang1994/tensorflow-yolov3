05b8fb1a9f155ba808ae4d60ff705e1665af3d87
YunYang1994 <YunYang1994@github.com>
2019-01-26 01:38:23 +0800

    image hw

--- video_demo.py
+++ video_demo.py
@@ -19,7 +19,7 @@ from PIL import Image
 from core import utils
 
 
-SIZE = [416, 416]
+IMAGE_H, IMAGE_W = 416, 416
 video_path = "./data/demo_data/road.mp4"
 video_path = 0 # use camera
 classes = utils.read_coco_names('./data/coco.names')
@@ -35,13 +35,13 @@ with tf.Session() as sess:
             image = Image.fromarray(frame)
         else:
             raise ValueError("No image!")
-        img_resized = np.array(image.resize(size=tuple(SIZE)), dtype=np.float32)
+        img_resized = np.array(image.resize(size=(IMAGE_H, IMAGE_W)), dtype=np.float32)
         img_resized = img_resized / 255.
         prev_time = time.time()
 
         boxes, scores = sess.run(output_tensors, feed_dict={input_tensor: np.expand_dims(img_resized, axis=0)})
         boxes, scores, labels = utils.cpu_nms(boxes, scores, num_classes, score_thresh=0.4, iou_thresh=0.5)
-        image = utils.draw_boxes(image, boxes, scores, labels, classes, SIZE, show=False)
+        image = utils.draw_boxes(image, boxes, scores, labels, classes, (IMAGE_H, IMAGE_W), show=False)
 
         curr_time = time.time()
         exec_time = curr_time - prev_time
