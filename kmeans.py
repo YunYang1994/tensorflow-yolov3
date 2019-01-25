cbc0c220a04f991b20c175fdf8c27ac61ba34144
YunYang1994 <YunYang1994@github.com>
2019-01-25 11:32:08 +0800

    k-means

---
+++ kmeans.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# coding=utf-8
+#================================================================
+#   Copyright (C) 2019 * Ltd. All rights reserved.
+#
+#   Editor      : VIM
+#   File name   : kmeans.py
+#   Author      : YunYang1994
+#   Created date: 2019-01-25 11:08:15
+#   Description :
+#
+#================================================================
+
+# import argparse
+
+annot_path = "./raccoon_dataset/labels.txt"
+boxes_data = open(annot_path, 'r').readlines()
+
