7acc83e9f0892cd89ad479cbb5159d3482737c05
YunYang1994 <YunYang1994@github.com>
2019-01-26 17:37:39 +0800

    I hate tensorflow

--- show_input_image.py
+++ show_input_image.py
@@ -25,7 +25,7 @@ SHUFFLE_SIZE = 1
 
 train_tfrecord = "/home/yang/test/VOC_DATA/train/voc_train.tfrecords"
 anchors        = utils.get_anchors('./data/voc_anchors.txt', IMAGE_H, IMAGE_W)
-classes = utils.read_coco_names('./data/coco.names')
+classes = utils.read_coco_names('./data/voc.names')
 num_classes = len(classes)
 
 parser   = Parser(IMAGE_H, IMAGE_W, anchors, num_classes, debug=True)
@@ -34,7 +34,7 @@ trainset = dataset(parser, train_tfrecord, BATCH_SIZE, shuffle=SHUFFLE_SIZE)
 is_training = tf.placeholder(tf.bool)
 example = trainset.get_next()
 
-for l in range(1):
+for l in range(10):
     image, boxes = sess.run(example)
     image, boxes = image[0], boxes[0]
 
