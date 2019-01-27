1e9b2c52b6666d52add062ec23b5bdb729b1cdef
YunYang1994 <YunYang1994@github.com>
2019-01-28 00:07:02 +0800

    I hate tensorflow

--- quick_test.py
+++ quick_test.py
@@ -22,7 +22,7 @@ classes = utils.read_coco_names('./data/raccoon.names')
 num_classes = len(classes)
 image_path = "./raccoon_dataset/images/raccoon-182.jpg"  # 181,
 img = Image.open(image_path)
-img_resized = np.array(img.resize(size=(IMAGE_H, IMAGE_W), dtype=np.float32)
+img_resized = np.array(img.resize(size=(IMAGE_H, IMAGE_W)), dtype=np.float32)
 img_resized = img_resized / 255.
 cpu_nms_graph = tf.Graph()
 
@@ -31,4 +31,4 @@ input_tensor, output_tensors = utils.read_pb_return_tensors(cpu_nms_graph, "./ch
 with tf.Session(graph=cpu_nms_graph) as sess:
     boxes, scores = sess.run(output_tensors, feed_dict={input_tensor: np.expand_dims(img_resized, axis=0)})
     boxes, scores, labels = utils.cpu_nms(boxes, scores, num_classes, score_thresh=0.3, iou_thresh=0.5)
-    image = utils.draw_boxes(img, boxes, scores, labels, classes, SIZE, show=True)
+    image = utils.draw_boxes(img, boxes, scores, labels, classes, [IMAGE_H, IMAGE_W], show=True)
