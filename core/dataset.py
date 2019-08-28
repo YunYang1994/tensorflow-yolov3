b8230181f570f1701d5f880a80d013efdcb73a10
Kashyap Jois <kjois@iprdgroup.com>
2019-08-28 20:57:35 +0530

    Added image augmentation

--- core/dataset.py
+++ core/dataset.py
@@ -18,7 +18,7 @@ import numpy as np
 import tensorflow as tf
 import core.utils as utils
 from core.config import cfg
-
+import math
 
 
 class Dataset(object):
@@ -156,17 +156,114 @@ class Dataset(object):
         line = annotation.split()
         image_path = line[0]
         if not os.path.exists(image_path):
-            raise KeyError("%s does not exist ... " %image_path)
+            image_path = image_path.replace("jpg","jpeg")
         image = np.array(cv2.imread(image_path))
-        bboxes = np.array([list(map(int, box.split(','))) for box in line[1:]])
+        bboxes = np.array([list(map(float, box.split(','))) for box in line[1:]],dtype=int)
 
         if self.data_aug:
             image, bboxes = self.random_horizontal_flip(np.copy(image), np.copy(bboxes))
             image, bboxes = self.random_crop(np.copy(image), np.copy(bboxes))
             image, bboxes = self.random_translate(np.copy(image), np.copy(bboxes))
+            fraction = 0.20  # must be < 1.0
+            img_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)  # hue, sat, val
+            S = img_hsv[:, :, 1].astype(np.float32)  # saturation
+            V = img_hsv[:, :, 2].astype(np.float32)  # value
 
+            a = (random.random() * 2 - 1) * fraction + 1
+            b = (random.random() * 2 - 1) * fraction + 1
+            S *= a
+            V *= b
+
+            img_hsv[:, :, 1] = S if a < 1 else S.clip(None, 255)
+            img_hsv[:, :, 2] = V if b < 1 else V.clip(None, 255)
+            cv2.cvtColor(img_hsv, cv2.COLOR_HSV2BGR, dst=image)
+        
         image, bboxes = utils.image_preporcess(np.copy(image), [self.train_input_size, self.train_input_size], np.copy(bboxes))
-        return image, bboxes
+#         print(bboxes)
+#         labels = bboxes.copy()
+#         labels[:, 0] = (bboxes[:, 0] - bboxes[:, 2] / 2)
+#         labels[:, 1] = (bboxes[:, 1] - bboxes[:, 3] / 2)
+#         labels[:, 2] = (bboxes[:, 0] + bboxes[:, 2] / 2)
+#         labels[:, 3] = (bboxes[:, 1] + bboxes[:, 3] / 2)
+        
+        imw,labs = self.random_affine(image, bboxes, degrees=(-180, 180), translate=(0.10, 0.10), scale=(0.80, 1.20))
+#         print(labs)
+        
+#         print(labels)
+        
+        return imw, labs
+    
+    
+    
+    def random_affine(self,img, targets=(), degrees=(-10, 10), translate=(.1, .1), scale=(.9, 1.1), shear=(-2, 2),
+                  borderValue=(127.5, 127.5, 127.5)):
+        # torchvision.transforms.RandomAffine(degrees=(-10, 10), translate=(.1, .1), scale=(.9, 1.1), shear=(-10, 10))
+        # https://medium.com/uruvideo/dataset-augmentation-with-random-homographies-a8f4b44830d4
+
+        if targets is None:
+            targets = []
+        border = 0  # width of added border (optional)
+        height = img.shape[0] + border * 2
+        width = img.shape[1] + border * 2
+
+        # Rotation and Scale
+        R = np.eye(3)
+        a = random.random() * (degrees[1] - degrees[0]) + degrees[0]
+#         a += random.choice([-180, -90, 0, 90])  # 90deg rotations added to small rotations
+        s = random.random() * (scale[1] - scale[0]) + scale[0]
+        R[:2] = cv2.getRotationMatrix2D(angle=a, center=(img.shape[1] / 2, img.shape[0] / 2), scale=s)
+
+        # Translation
+        T = np.eye(3)
+        T[0, 2] = (random.random() * 2 - 1) * translate[0] * img.shape[0] + border  # x translation (pixels)
+        T[1, 2] = (random.random() * 2 - 1) * translate[1] * img.shape[1] + border  # y translation (pixels)
+
+        # Shear
+        S = np.eye(3)
+        S[0, 1] = math.tan((random.random() * (shear[1] - shear[0]) + shear[0]) * math.pi / 180)  # x shear (deg)
+        S[1, 0] = math.tan((random.random() * (shear[1] - shear[0]) + shear[0]) * math.pi / 180)  # y shear (deg)
+
+        M = S @ T @ R  # Combined rotation matrix. ORDER IS IMPORTANT HERE!!
+        imw = cv2.warpPerspective(img, M, dsize=(width, height), flags=cv2.INTER_LINEAR,
+                                  borderValue=borderValue)  # BGR order borderValue
+
+        # Return warped points also
+        if len(targets) > 0:
+            n = targets.shape[0]
+            points = targets[:, 0:4].copy()
+            area0 = (points[:, 2] - points[:, 0]) * (points[:, 3] - points[:, 1])
+
+            # warp points
+            xy = np.ones((n * 4, 3))
+            xy[:, :2] = points[:, [0, 1, 2, 3, 0, 3, 2, 1]].reshape(n * 4, 2)  # x1y1, x2y2, x1y2, x2y1
+            xy = (xy @ M.T)[:, :2].reshape(n, 8)
+
+            # create new boxes
+            x = xy[:, [0, 2, 4, 6]]
+            y = xy[:, [1, 3, 5, 7]]
+            xy = np.concatenate((x.min(1), y.min(1), x.max(1), y.max(1))).reshape(4, n).T
+
+            # # apply angle-based reduction of bounding boxes
+            # radians = a * math.pi / 180
+            # reduction = max(abs(math.sin(radians)), abs(math.cos(radians))) ** 0.5
+            # x = (xy[:, 2] + xy[:, 0]) / 2
+            # y = (xy[:, 3] + xy[:, 1]) / 2
+            # w = (xy[:, 2] - xy[:, 0]) * reduction
+            # h = (xy[:, 3] - xy[:, 1]) * reduction
+            # xy = np.concatenate((x - w / 2, y - h / 2, x + w / 2, y + h / 2)).reshape(4, n).T
+
+            # reject warped points outside of image
+            xy[:, [0, 2]] = xy[:, [0, 2]].clip(0, width)
+            xy[:, [1, 3]] = xy[:, [1, 3]].clip(0, height)
+            w = xy[:, 2] - xy[:, 0]
+            h = xy[:, 3] - xy[:, 1]
+            area = w * h
+            ar = np.maximum(w / (h + 1e-16), h / (w + 1e-16))
+            i = (w > 4) & (h > 4) & (area / (area0 + 1e-16) > 0.1) & (ar < 10)
+
+            targets = targets[i]
+            targets[:, 0:4] = xy[i]
+        return imw, targets
 
     def bbox_iou(self, boxes1, boxes2):
 
@@ -200,7 +297,6 @@ class Dataset(object):
         for bbox in bboxes:
             bbox_coor = bbox[:4]
             bbox_class_ind = bbox[4]
-
             onehot = np.zeros(self.num_classes, dtype=np.float)
             onehot[bbox_class_ind] = 1.0
             uniform_distribution = np.full(self.num_classes, 1.0 / self.num_classes)
