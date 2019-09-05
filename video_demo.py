eacf8c163198db98d6e0bc46fc0af463f11f5a7b
Tei Jeong <taeheej@google.com>
2019-09-06 00:46:03 +0900

    Eliminated 5D tensor usage
    Eliminated 5D tensor usage in decode function
    for better compatibility.

--- video_demo.py
+++ video_demo.py
@@ -19,7 +19,7 @@ import tensorflow as tf
 from PIL import Image
 
 
-return_elements = ["input/input_data:0", "pred_sbbox/concat_2:0", "pred_mbbox/concat_2:0", "pred_lbbox/concat_2:0"]
+return_elements = ["input/input_data:0", "pred_sbbox/Reshape:0", "pred_mbbox/Reshape:0", "pred_lbbox/Reshape:0"]
 pb_file         = "./yolov3_coco.pb"
 video_path      = "./docs/images/road.mp4"
 # video_path      = 0
