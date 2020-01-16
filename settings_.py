c220b97101de48cb62c4ae48261b84ebba735d45
kashyap jois <kjois@iprdgroup.com>
2020-01-16 17:28:51 +0530

    Add setting and latest model

---
+++ settings_.py
@@ -0,0 +1,5 @@
+RDT_GIT_ROOT="C:/Users/Kashyap/bkp/source/repos/rdt-reader"
+FLU_AUDERE_PATH="C:/Users/Kashyap/bkp/source/repos/rdt-reader/tensorflow-yolov3-models/models/Flu_audere/1"
+FLU_AUDERE_LINE_PATH="C:/Users/Kashyap/bkp/source/repos/rdt-reader/tensorflow-yolov3-models/models/Flu_audere_line/11"
+YOLO_MODEL_VER         = 1
+LINE_MODEL_VER         = 5
